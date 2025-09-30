b6517dce8b3f872964670f9d421df2b074ded043
gsiri007 <216904534+gsiri007@users.noreply.github.com>
2025-09-30 18:17:13 +0530

    basic working implementation

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
+from os import path
 import tkinter
+from tkinter import messagebox
 import subprocess
-from customtkinter import CTkFrame, CTkImage, CTkButton, CTkLabel, CTkScrollableFrame, filedialog
+from customtkinter import LEFT, CTkFrame, CTkImage, CTkButton, CTkLabel, CTkScrollableFrame, filedialog
 from PIL import Image
 
 root = tkinter.Tk()
 root.title('wallker')
 root.config(bg='black')
 
+# image buttons
+image_paths   = []
+image_buttons = []
+
 # functions
-def set_directory():
+def set_directory() -> None:
     directory = filedialog.askdirectory()
     get_images(directory)
+
+def set_wallpaper(path: str) -> None:
+    response = messagebox.askyesno(
+        message= 'Are you sure you want to set this image as the wallpaper?',
+        icon='question'
+    )
+
+    if response:
+        subprocess.run(
+            [
+                f'hyprctl hyprpaper preload {path} && hyprctl hyprpaper wallpaper ", {path}"',
+            ],
+            shell=True,
+            stdout=subprocess.DEVNULL
+            
+        )
+    else:
+        pass
+
+
+
+
+
+def set_image_btn_command():
+    for btn in image_buttons:
+        index = image_buttons.index(btn)
+        btn.configure(command=lambda i=index: set_wallpaper(image_paths[i]))
+        btn.pack(pady=10)
 
 def get_images(directory: str):
     result = subprocess.run(
@@ -23,11 +57,17 @@ def get_images(directory: str):
     images = result.split()
 
     for image in images:
-        file_path = f'{directory}/{image}'
-        img = Image.open(file_path)
+        image_path = f'{directory}/{image}'
+        image_paths.append(image_path)
+        img = Image.open(image_path)
         ctk_img = CTkImage(light_image=img, dark_image=img, size=(250,250))
-        img_label = CTkButton(body_frame, image=ctk_img, text="")
-        img_label.pack(pady=10)
+        image_buttons.append(CTkButton(
+            body_frame, 
+            text="",
+            image=ctk_img, 
+        ))
+
+    set_image_btn_command()
 
 
 # frames
@@ -37,14 +77,13 @@ body_frame = CTkScrollableFrame(root)
 header_frame.pack()
 body_frame.pack(fill='both', expand=True)
 
-# gui
+# widgets
 select_directory_btn = CTkButton(
     header_frame,
     text='Select',
     command=set_directory
 )
-select_directory_btn.pack()
-
+select_directory_btn.pack(side=LEFT)
 
 
 root.mainloop()
