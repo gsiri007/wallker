fb3c0718e9c96ae7d64e75f6888c2c797cfc12b8
gsiri007 <216904534+gsiri007@users.noreply.github.com>
2025-10-03 09:30:01 +0530

    switch image button placement from pack to grid layout

--- main.py
+++ main.py
@@ -56,7 +56,9 @@ def get_images_async(directory: str):
 
     images = result.split()
 
-    for image in images:
+    row = 0
+    column = 0
+    for index, image in enumerate(images):
         try:
             #TODO:check if file is an image format, skip if not a valid format
             image_path = f'{directory}/{image}'
@@ -65,9 +67,15 @@ def get_images_async(directory: str):
             img.thumbnail((250, 250), Image.Resampling.BICUBIC)
             ctk_img = CTkImage(light_image=img, dark_image=img, size=(250,250))
 
-            root.after(0, set_image_btn, image_path, ctk_img)
+            root.after(0, set_image_btn, image_path, ctk_img, (row, column))
         except Exception as e:
             print(f'ERROR: {e}')
+
+        column += 1
+        if column == 6:
+            column = 0
+            row += 1
+
 
 
 #############################################
@@ -82,23 +90,24 @@ header_frame.pack(pady=20)
 body_frame.pack(fill='both', expand=True)
 
 # widgets
-select_directory_btn = CTkButton(
+open_directory_btn = CTkButton(
     header_frame,
-    text='Select',
+    text='Open',
     command=set_directory,
     bg_color='grey',
     fg_color='grey',
     corner_radius=0
 )
-select_directory_btn.pack()
+open_directory_btn.pack()
 
-def set_image_btn(image_path: str, ctk_img: CTkImage):
+def set_image_btn(image_path: str, ctk_img: CTkImage, cell: tuple):
     img_btn = CTkButton(
         body_frame, 
         text="",
         image=ctk_img, 
         command=lambda p=image_path:set_wallpaper(p)
     )
-    img_btn.pack(pady=10)
+    row, column = cell
+    img_btn.grid(row=row, column=column, padx=25, pady=10)
 
 root.mainloop()
